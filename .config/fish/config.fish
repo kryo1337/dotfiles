8356e4e03354e9d72e46e999ad9ce0289794eae2
kryo1337 <kryo1337@gmail.com>
2025-01-27 18:14:57 +0100

    initial

---
+++ .config/fish/config.fish
@@ -0,0 +1,82 @@
+set -g fish_greeting
+
+if status is-interactive
+    starship init fish | source
+end
+
+# List Directory
+alias l='eza -lh --icons=auto' # long list
+alias ls='eza -1a --icons=auto' # short list
+alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
+alias ld='eza -lhD --icons=auto' # long list dirs
+alias lt='eza --icons=auto --tree' # list folder as tree
+
+# Handy change dir shortcuts
+abbr .. 'cd ..'
+abbr .2 'cd ../..'
+abbr .3 'cd ../../..'
+abbr .4 'cd ../../../..'
+abbr .5 'cd ../../../../..'
+
+# Always mkdir a path (this doesn't inhibit functionality to make a single dir)
+abbr mkdir 'mkdir -p'
+
+set -x EDITOR "vim"
+
+function fish_prompt
+  set_color --bold 4086ef
+
+  set transformed_pwd (prompt_pwd | string replace -r "^~" (set_color --bold 06b6d4)"~"(set_color --bold 3b82f6))
+
+  echo -n $transformed_pwd
+
+  # Git branch
+  if git rev-parse --is-inside-work-tree >/dev/null 2>&1
+    echo -n " "
+    echo -n (set_color --bold 4338ca)"("
+    set_color f0abfc
+    echo -n (git branch --show-current)
+    echo -n (set_color --bold 4338ca)")"
+  end
+
+  echo -n " "
+  echo -n (set_color --bold 14b8a6)"→ "
+  set_color normal
+end
+
+# TokyoNight Color Palette from https://github.com/folke/tokyonight.nvim/blob/main/extras/fish/tokyonight_storm.fish
+set -l foreground c0caf5
+# changed from default
+set -l selection 6366f1
+# changed from default
+set -l comment 737373
+set -l red f7768e
+set -l orange ff9e64
+set -l yellow e0af68
+set -l green 9ece6a
+set -l purple 9d7cd8
+set -l cyan 7dcfff
+set -l pink bb9af7
+
+# Syntax Highlighting Colors
+set -g fish_color_normal $foreground
+set -g fish_color_command $cyan
+set -g fish_color_keyword $pink
+set -g fish_color_quote $yellow
+set -g fish_color_redirection $foreground
+set -g fish_color_end $orange
+set -g fish_color_error $red
+set -g fish_color_param $purple
+set -g fish_color_comment $comment
+set -g fish_color_selection --background=$selection
+set -g fish_color_search_match --background=$selection
+set -g fish_color_operator $green
+set -g fish_color_escape $pink
+set -g fish_color_autosuggestion $comment
+
+# Completion Pager Colors
+set -g fish_pager_color_progress $comment
+set -g fish_pager_color_prefix $cyan
+set -g fish_pager_color_completion $foreground
+set -g fish_pager_color_description $comment
+set -g fish_pager_color_selected_background --background=$selection
